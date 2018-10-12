ab3f9fb1d81f442de58106993a7fe425063401ae
Victor Chang <v.chang.yu@gmail.com>
2018-10-12 18:07:38 -0300

    login controller started

---
+++ CVApp/Login/LoginController.swift
@@ -0,0 +1,78 @@
+//
+//  LoginController.swift
+//  CVApp
+//
+//  Created by Victor Chang on 12/10/2018.
+//  Copyright © 2018 Victor Chang. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class  LoginController: UIViewController {
+
+    let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return textField
+    }()
+    
+    let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        textField.borderStyle = .roundedRect
+        textField.isSecureTextEntry = true
+        textField.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        textField.font = UIFont.systemFont(ofSize: 14)
+        textField.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return textField
+    }()
+    
+    @objc fileprivate func handleTextInputChange() {
+        print("handleTextInputChange Triggered")
+    }
+    
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self  , action: #selector(handleLogin), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func handleLogin() {
+        print("Login Triggered")
+    }
+    
+    let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account?  ", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.lightGray])
+        
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        attributedTitle.append(NSMutableAttributedString(string: "Sign Up", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14), NSAttributedString.Key.foregroundColor: UIColor.rgb(red: 17, green: 154, blue: 237)]))
+        button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
+        return button
+    }()
+    
+    @objc fileprivate func handleShowSignUp() {
+        print("SignUp Button Triggered")
+        
+        
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
