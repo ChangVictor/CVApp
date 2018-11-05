8075e0daf1baeb6fbf2ec8484831e6f73014a64d
Victor Chang <v.chang.yu@gmail.com>
2018-11-05 20:32:55 -0300

    Victor header started

--- CVApp/Controllers/Victor/VictorProfileHeader.swift
+++ CVApp/Controllers/Victor/VictorProfileHeader.swift
@@ -7,8 +7,29 @@
 //
 
 import UIKit
+import Firebase
 
 class VictorProfileHeader: UICollectionViewCell {
+    
+    let profileImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.backgroundColor = .gray
+        return imageView
+    }()
+    
+    let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Victor Chang"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    let infoLabel: UILabel = {
+        let label = UILabel()
+        let attributedText = NSMutableAttributedString(string: "Victor's Biography here...\n", attributes: [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 14)])
+        attributedText.append(NSAttributedString(string: "Insert some text in here", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, kCTFontAttributeName as NSAttributedString.Key: UIFont.systemFont(ofSize: 14)]))
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
