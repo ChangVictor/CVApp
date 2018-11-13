72f4c44140132d823e860b4a8b070cf0d2d47a68
Victor Chang <v.chang.yu@gmail.com>
2018-11-13 19:37:10 -0300

    Victor profile header info added. need autolayout fixes

--- CVApp/Controllers/Victor/VictorProfileHeader.swift
+++ CVApp/Controllers/Victor/VictorProfileHeader.swift
@@ -25,25 +25,32 @@ class VictorProfileHeader: UICollectionViewCell {
     
     let personalInfoLabel: UILabel = {
         let label = UILabel()
-        label.text = "Born 8 August 1990 /n Based in Buenos Aires"
-        label.font = UIFont.systemFont(ofSize: 12)
-        label.textColor = .lightGray
+        let attributedText = NSMutableAttributedString(string: "iOS Development | Tech Enthusiast", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, kCTFontAttributeName as NSAttributedString.Key: UIFont.systemFont(ofSize: 12)])
+        attributedText.append(NSAttributedString(string: "\n\n", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 3)]))
+        attributedText.append(NSAttributedString(string: "Born 8 August 1990", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, kCTFontAttributeName as NSAttributedString.Key: UIFont.systemFont(ofSize: 12)]))
+        attributedText.append(NSAttributedString(string: "\n\n", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 3)]))
+        attributedText.append(NSAttributedString(string: "Based in Buenos Aires, Argentina", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, kCTFontAttributeName as NSAttributedString.Key: UIFont.systemFont(ofSize: 12)]))
+
+        label.attributedText = attributedText
+        label.numberOfLines = 0
+        label.textAlignment = .left
         return label
     }()
     
     let nameLabel: UILabel = {
         let label = UILabel()
         label.text = "Victor Chang"
-        label.font = UIFont.boldSystemFont(ofSize: 16)
-        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.textAlignment = .left
+        
         return label
     }()
     
     let infoLabel: UILabel = {
         let label = UILabel()
-        let attributedText = NSMutableAttributedString(string: "Former Business Analyst transitioning into iOS Developer", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 14)])
-        attributedText.append(NSAttributedString(string: "\n\n", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 4)]))
-        attributedText.append(NSAttributedString(string: "Constantly striving to learn new technologies and good practices in order to create nice appealing UI while writing highly readable clean code.", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, kCTFontAttributeName as NSAttributedString.Key: UIFont.systemFont(ofSize: 14)]))
+        let attributedText = NSMutableAttributedString(string: "Former Business Analyst transitioning into iOS Developer", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 13)])
+        attributedText.append(NSAttributedString(string: "\n\n", attributes: [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 6)]))
+        attributedText.append(NSAttributedString(string: "Constantly striving to learn new technologies and good practices in order to create nice appealing UI while writing highly readable clean code.", attributes: [NSAttributedString.Key.foregroundColor: UIColor.darkGray, kCTFontAttributeName as NSAttributedString.Key: UIFont.systemFont(ofSize: 13)]))
         label.attributedText = attributedText
         label.numberOfLines = 0
         label.textAlignment = .center
@@ -108,7 +115,7 @@ class VictorProfileHeader: UICollectionViewCell {
         addSubview(topDividerView)
         addSubview(bottomDividerView)
         
-        stackView.anchor(top: nil, left: leftAnchor, bottom: self.bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        stackView.anchor(top: personalInfoLabel.bottomAnchor, left: leftAnchor, bottom: self.bottomAnchor, right: rightAnchor, paddingTop: 16, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
         topDividerView.anchor(top: stackView.topAnchor, left: leftAnchor, bottom: nil, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
         bottomDividerView.anchor(top: stackView.bottomAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 0.5)
         
@@ -119,17 +126,19 @@ class VictorProfileHeader: UICollectionViewCell {
         addSubview(nameLabel)
         addSubview(infoLabel)
         addSubview(profileImageView)
+        addSubview(personalInfoLabel)
         
-        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 110, height: 110)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 0, width: 100, height: 100)
         setupProfileImage()
         
-        nameLabel.anchor(top: topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 0)
-        infoLabel.anchor(top: nameLabel.bottomAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 0)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, left: leftAnchor, bottom: nil, right: infoLabel.leftAnchor, paddingTop: 8, paddingLeft: 8, paddingBottom: 0, paddingRight: 8, width: 0, height: 0)
+        infoLabel.anchor(top: self.topAnchor, left: profileImageView.rightAnchor, bottom: nil, right: self.rightAnchor, paddingTop: 4, paddingLeft: 12, paddingBottom: 0, paddingRight: 12, width: 0, height: 0)
+        personalInfoLabel.anchor(top: nameLabel.bottomAnchor, left: leftAnchor, bottom: nil, right: nil, paddingTop: 4, paddingLeft: 8, paddingBottom: 0, paddingRight: 12, width: 0, height: 0)
         setupSocialMediaBar()
     }
     
     fileprivate func setupProfileImage() {
-        profileImageView.layer.cornerRadius = 110 / 2
+        profileImageView.layer.cornerRadius = 100 / 2
         profileImageView.layer.masksToBounds = true
         profileImageView.clipsToBounds = true
 //        profileImageView.layer.borderColor = UIColor.rgb(red: 224, green: 57, blue: 62).cgColor
